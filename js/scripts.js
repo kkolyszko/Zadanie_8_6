6d103d69914e9e0f17a9407af3d9d20d7d6bf428
kkolyszko <katarzyna.kolyszko@gmail.com>
2017-10-03 20:42:53 +0200

    poprwa literówki i dodanie console.log

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,17 @@ b = prompt('Enter b');
 operation = (a * a) - (2 * a * b) + (b * b);
 
 if (operation > 0) {
-    plusOrMinus = "positive"
-}  else if (operation < 0) {
-    plusOrMinus = "negative"
+    plusOrMinus = "positive";
+    console.log("wynik dodatni");
+} else if (operation < 0) {
+    plusOrMinus = "negative";
+    console.log("wynik ujemny");
 } else {
     plusOrMinus = "nor positive neither negative"
+    console.log("wynik równy zero");
 };
-        
-              
+
+
 document.getElementById("result").innerHTML = 'The result of the operation is:<br> ' + operation;
 
 document.getElementById("result2").innerHTML = 'The result is ' + plusOrMinus;
